56b6ae3e8b92b83befd0acf94c1a4d2581019fe0
tvang83 <62636069+tvang83@users.noreply.github.com>
2020-05-01 08:06:38 -0500

    update

--- Sandbox-C#/Sandbox/CustomList.cs
+++ Sandbox-C#/Sandbox/CustomList.cs
@@ -10,8 +10,17 @@ namespace Sandbox
     {
         // member variables (HAS A)
         private T[] items;  //List array variable 
-        private  int count;  //for read-only count property
+        private int count;  //for read-only count property
         private int capacity; // for capacity property
+        //public int[] one = { 1, 3, 5 };
+        public int[] two = { 2, 4, 6 };
+        public int[] minusTwo = { 2, 1, 6 };
+        public int listResults;
+        public int listResultsOutput;
+        public int minusListResults;
+
+        List<int> one = new List<int>() { 1, 3, 5 };
+        //List<int> two = new List<int>() { 2, 4, 6 };
 
         public int Count
         {
@@ -55,11 +64,13 @@ namespace Sandbox
             count = 0;
             capacity = 4;
             items = new T[capacity];
+           // listResults = listResultsOutput;
+
         }
 
         // member methods (CAN DO)
 
-        public void Add(T value) //what's being passed in when this method is called.
+        public void Add(T value) //what's being passed in when this method is called.  Need to add if statement to 
         {
             items[count] = value;
             count++;
@@ -70,9 +81,21 @@ namespace Sandbox
             items[count] = value;
             count--;
         }
-        //public override ChangeToString()  //change List items to string, this will throw an error with the UnitTest class ClassList<>
-        //{
-        //   return items.ToString();
-        //}
+        public override string ToString()  //change List items to string, this will throw an error with the UnitTest class ClassList<>
+        {
+            return items.ToString();
+        }
+
+        public static CustomList<T> operator+ (CustomList<T> one, CustomList<T> two)  //crete two lists with list values and overload + operator, combining two lists
+        {
+            CustomList<T> listResults = one + two;
+            return listResults;
+        }
+
+        public static CustomList<T> operator- (CustomList<T> one, CustomList<T> minusTwo)  //crete two lists with list values and overload + operator, combining two lists
+        {
+            CustomList<T> minusListResults = one - minusTwo;
+            return minusListResults;
+        }
     }
 }
